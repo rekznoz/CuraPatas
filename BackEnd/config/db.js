ec9c275d6931a91b01238cf5d0bbcda47f327003
naatii <jcorbre855@g.educaand.es>
2024-12-11 16:26:00 +0100

    refator animal controller

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        console.log(process.env.MONGODB_URI)
         await mongoose.connect(process.env.MONGODB_URI);
         console.log('Conectado a MongoDB');
     } catch (err) {
--- BackEnd/controllers/animalesController.js
+++ BackEnd/controllers/animalesController.js
@@ -1,24 +1,6 @@
 const mongoose = require('mongoose');
 const Animales = require('../models/Animals'); // Suponiendo que el modelo está en models/Animals.js
 
-exports.createAnimal = async (req, res) => {
-    const { nombre, especie, raza, edad, estadoSalud, duenio, fechaRegistro } = req.body;
-
-    // Validación de campos
-    if (!nombre || !especie || !raza || !edad || !estadoSalud || !duenio || !fechaRegistro) {
-        return res.status(400).json({ error: 'Todos los campos son requeridos' });
-    }
-
-    try {
-        // Crear instancia del modelo
-        const animal = new Animales({ nombre, especie, raza, edad, estadoSalud, duenio, fechaRegistro });
-        await animal.save();
-        res.json({ message: "Animal registrado con éxito", animal });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al agregar el animal', details: error.message });
-    }
-};
-
 exports.getAnimales = async (req, res) => {
     try {
         const { page = 1, limit = 10 } = req.query;
@@ -37,17 +19,35 @@ exports.getAnimales = async (req, res) => {
     }
 };
 
-exports.removeAnimal = async (req, res) => {
+exports.createAnimal = async (req, res) => {
     const { nombre, especie, raza, edad, estadoSalud, duenio, fechaRegistro } = req.body;
 
     // Validación de campos
-    if (!nombre) {
+    if (!nombre || !especie || !raza || !edad || !estadoSalud || !duenio || !fechaRegistro) {
+        return res.status(400).json({ error: 'Todos los campos son requeridos' });
+    }
+
+    try {
+        // Crear instancia del modelo
+        const animal = new Animales({ nombre, especie, raza, edad, estadoSalud, duenio, fechaRegistro });
+        await animal.save();
+        res.json({ message: "Animal registrado con éxito", animal });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al agregar el animal', details: error.message });
+    }
+};
+
+exports.removeAnimal = async (req, res) => {
+    const { id } = req.body;
+
+    // Validación de campos
+    if (!id) {
         return res.status(400).json({ error: 'El nombre es requerido para eliminar un animal' });
     }
 
     try {
         // Buscar y eliminar el animal por nombre
-        const animal = await Animales.findOneAndDelete({ nombre });
+        const animal = await Animales.findByIdAndDelete(id);
 
         if (!animal) {
             return res.status(404).json({ error: 'Animal no encontrado' });
