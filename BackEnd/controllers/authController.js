e1f1aefdae8a765a69f57f0f1eac163f58929776
naatii <jcorbre855@g.educaand.es>
2024-12-13 08:55:44 +0100

    Añadida un breve analisis de mercado

--- BackEnd/controllers/authController.js
+++ BackEnd/controllers/authController.js
@@ -1,42 +1,44 @@
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const { generateToken } = require('../utils/tokenUtils');
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { generateToken } = require("../utils/tokenUtils");
 
 exports.register = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    if (!username || !password) {
-        return res.status(400).json({ error: 'El nombre de usuario y la contraseña son requeridos' });
-    }
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "El nombre de usuario y la contraseña son requeridos" });
+  }
 
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ username, password: hashedPassword });
-        await user.save();
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
 
-        res.json({ message: 'Usuario registrado!' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al registrar el usuario' });
-    }
+    res.json({ message: "Usuario registrado!" });
+  } catch (error) {
+    res.status(500).json({ error: "Error al registrar el usuario" });
+  }
 };
 
 exports.login = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    try {
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(400).json({ error: 'Usuario no encontrado' });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ error: 'Contraseña incorrecta' });
-        }
-
-        const token = generateToken(user._id);
-        res.json({ token });
-    } catch (error) {
-        res.status(500).json({ error: 'Error en la autenticación' });
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ error: "Usuario no encontrado" });
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Contraseña incorrecta" });
+    }
+
+    const token = generateToken(user._id);
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: "Error en la autenticación" });
+  }
 };
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CuraPatas
 
+![Portada CuraPatas](./assets/PortadaCuraPatas.webp)
+
 ## Descripción de la funcionalidad principal.
 
 **Registro y Perfiles de Usuarios:** Usuarios: Dueños de mascotas, veterinarios y voluntarios con perfiles personalizados con detalles como mascotas registradas, servicios ofrecidos o necesidades específicas.
@@ -10,6 +12,9 @@ La idea de CuraPatas surge de la necesidad de unificar en una sola plataforma lo
 
 ## Análisis de mercado.
 
+Investigando un poco nos damos cuenta que existen aplicaciones similares como por ejemplo [datapest](https://www.siacyl.org/public/datapetshelp.aspx) en la que las funcionalidades principales, como se describe en esta web, es la consulta de datos de identificación de mascotas activas en la base de datos. 
+
+Lo que nos diferencia de aplicaciones como éstas es que nuestra aplicación no se rige tan solo a una comunidad como se aclara en DataPests, dando un beneficio y aporte mayor a todos aquellos dueños de mascotas o animales, además de que en **CuraPatas**, vamos un paso más allá dando al usuario un aporte mayor pudiendo tener información más releante acerca del cuidado y mentenimiento de sus mascotas, consultorías con veterinarios voluntarios y productos para la correcta alimentación y cuidado de los mismos. El objetivo principal de **CuraPatas** es ayudar al cuidado de éstos dando una mayor calidad de vida a todos los animales con dueños mal informados o inexpertos.
 
 
 ## Funcionalidades clave.
