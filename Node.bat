81b6f1d4974d3c13b016103d6841d255f75611c4
naatii <jcorber855@gmail.com>
2025-01-08 09:30:38 +0100

    Arreglado el proyecto de mierda :D

--- Node.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Cambiar al directorio de tu aplicación
-cd ./BackEnd
-
-:: Iniciar el servidor con Node.js
-node index.js
-
-:: Mantener la ventana abierta si hay un error
-pause
--- React.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Cambiar al directorio de tu aplicación
-cd ./FrontEnd
-
-:: Iniciar el servidor con Node.js
-npm run dev
-
-:: Mantener la ventana abierta si hay un error
-pause
