011aaa9a01d6def2d244f5a4ac16d9052738aab8
naatii <jcorber855@gmail.com>
2025-01-09 19:28:44 +0100

    Creado el update de /animales

--- Servidor/routes/animalesRoutes.js
+++ Servidor/routes/animalesRoutes.js
@@ -4,8 +4,6 @@ const authenticate = require('../middlewares/authMiddleware');
 const { createAnimal, getAnimales, removeAnimal, updateAnimal } = require('../controllers/animalesController');
 
 router.post('/', createAnimal);
-router.get('/', getAnimales);
-router.delete("/", removeAnimal)
-router.put("/", updateAnimal)
+
 
 module.exports = router;
