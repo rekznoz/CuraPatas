db3f72788ce1ec2cde6be3e28c9e6fb594539ce7
RekZNoZ <rafikicroz@hotmail.com>
2025-01-11 23:01:23 +0100

    Update LayoutPublic.jsx

--- src/layouts/LayoutPublic.jsx
+++ src/layouts/LayoutPublic.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Outlet} from "react-router-dom";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+import Login from "../components/Login.jsx";
 
 function LayoutPublic() {
     return (
@@ -11,6 +12,7 @@ function LayoutPublic() {
                 <Outlet/>
             </main>
             <Footer/>
+            <Login/>
         </>
     )
 }
