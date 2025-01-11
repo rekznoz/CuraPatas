4dff3c8cf4c557a0ad1ba266f3cbe74b2a570ba7
RekZNoZ <rafikicroz@hotmail.com>
2025-01-11 23:01:07 +0100

    Create Inicio.jsx

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Inicio() {
+    return (
+        <>
+            <div className="area1">
+                <h1>Area 1</h1>
+            </div>
+            <div className="area2">
+                <h1>Area 2</h1>
+            </div>
+            <div className="area3">
+                <h1>Area 3</h1>
+            </div>
+        </>
+    )
+}
