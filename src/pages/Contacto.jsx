592d7f05466cc748483bb021f603cd6457acfa50
RekZNoZ <rafikicroz@hotmail.com>
2025-01-11 23:01:18 +0100

    Create Contacto.jsx

---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,18 @@
+import Formulario from "../components/Formulario.jsx";
+
+export default function Contacto() {
+    return (
+        <>
+            <h1>Esto es el contacto</h1>
+            <div className="area1">
+                <h1>Area 1</h1>
+            </div>
+            <div className="area2">
+                <Formulario />
+            </div>
+            <div className="area3">
+                <h1>Area 3</h1>
+            </div>
+        </>
+    )
+}
