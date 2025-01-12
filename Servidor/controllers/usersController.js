26aef6cffa036268f89c7def608c78f96e829020
JoseSalazar86 <jasr1086@gmail.com>
2025-01-12 18:54:44 +0100

    update

--- Servidor/controllers/usersController.js
+++ Servidor/controllers/usersController.js
@@ -8,28 +8,10 @@ exports.createUser = async (req, res) => {
     if (!username || !animales || !email || !rol ) {
         return res.status(400).json({ error: 'Todos los campos son requeridos' });
     }
-    // Validar formato del email
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        return res.status(400).json({ error: "El correo electrónico no es válido" });
-    }
-
-    // Validar la seguridad de la contraseña
-    if (password.length < 8) {
-        return res.status(400).json({ error: "La contraseña debe tener al menos 8 caracteres" });
-    }
-
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    if (!passwordRegex.test(password)) {
-        return res.status(400).json({
-            error: "La contraseña debe contener al menos una letra, un número y un carácter especial",
-        });
-    }
-
     try {
         // Verificar si el email ya está registrado
         const existingUser = await User.findOne({email});
-        if (!existingUser) {
+        if (existingUser) {
             return res.status(400).json({ error: "El email ya está registrado" });
         }
 
