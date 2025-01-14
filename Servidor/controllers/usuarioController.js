5a91cd82a190afd6b696b66e941aa6dee43f70c1
RekZNoZ <rafikicroz@hotmail.com>
2025-01-14 20:55:02 +0100

    Update usuarioController.js

--- Servidor/controllers/usuarioController.js
+++ Servidor/controllers/usuarioController.js
@@ -5,33 +5,35 @@ const Bycript = require('bcryptjs')
 var currentDate = new Date();
 
 exports.crearUsuario = async (req, res) => {
-    const { username, email, contraseña } = req.body;
-    
+    const {username, email, contraseña} = req.body;
+
     // Validación de campos
-    if (!username  || !email || !contraseña ) {
-        return res.status(400).json({ error: 'Todos los campos son requeridos' });
+    if (!username || !email || !contraseña) {
+        return res.status(400).json({error: 'Todos los campos son requeridos'});
     }
-     try {
+    try {
         // Verificar si el email ya está registrado
         const existingUser = await User.findOne({email});
         if (existingUser) {
-            return res.status(400).json({ error: "El email ya está registrado" });
+            return res.status(400).json({error: "El email ya está registrado"});
         }
-         const user = new Users({ username, email, rol:"Usuario", fechaRegistro: currentDate.toLocaleDateString()});
+        const user = new Users({username, email, rol: "Usuario", fechaRegistro: currentDate.toLocaleDateString()});
         await user.save();
-        res.json({ message: "Usuario registrado con éxito", user });
+        res.json({message: "Usuario registrado con éxito", user});
     } catch (error) {
-        res.status(500).json({ error: 'Error al agregar el animal', details: error.message });
+        res.status(500).json({error: 'Error al agregar el animal', details: error.message});
     }
 };
+
 exports.login = async (req, res) => {
-    const { username, animales, email, rol } = req.body;
+    const {username, animales, email, rol} = req.body;
     // Implementar validación aquí
-    
+
 }
+
 exports.obtenerUsuarios = async (req, res) => {
     try {
-        const { page = 1, limit = 10 } = req.query;
+        const {page = 1, limit = 10} = req.query;
 
         // Convertir valores de paginación a números
         const pageNumber = Math.max(1, parseInt(page, 10));
@@ -43,7 +45,7 @@ exports.obtenerUsuarios = async (req, res) => {
 
         res.json(usuarios);
     } catch (error) {
-        res.status(500).json({ error: 'Error al obtener los animales', details: error.message });
+        res.status(500).json({error: 'Error al obtener los animales', details: error.message});
     }
 };
 
@@ -52,23 +54,20 @@ exports.editarUsuario = async (req, res) => {
 }
 
 exports.eliminarUsuario = async (req, res) => {
-    const usuario = { username } = req.body;
+    const usuario = {username} = req.body;
     // Validación de campos
     if (!username) {
-        return res.status(400).json({ error: 'El nombre es requerido para eliminar un animal' });
+        return res.status(400).json({error: 'El nombre es requerido para eliminar un animal'});
     }
 
     try {
         // Buscar y eliminar el usuario por nombre
         const nombreUsuario = await Users.find(usuario);
-        const { _id } = nombreUsuario[0]
-        const UsuarioBorrado = await Users.findByIdAndDelete({ _id });
-        
-        res.json({ message: "Usuario borrado con éxito", UsuarioBorrado });
+        const {_id} = nombreUsuario[0]
+        const UsuarioBorrado = await Users.findByIdAndDelete({_id});
+
+        res.json({message: "Usuario borrado con éxito", UsuarioBorrado});
     } catch (error) {
-        res.status(500).json({ error: 'Error al borrar el animal', details: error.message });
+        res.status(500).json({error: 'Error al borrar el animal', details: error.message});
     }
 }
-
-};
-
