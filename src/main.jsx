a328d9a243914f5e1ea35b7a5232e23fc96014ba
RekZNoZ <rafikicroz@hotmail.com>
2025-01-15 19:37:07 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import {RouterProvider} from "react-router-dom";
 import {router} from "./router/index.jsx"; // Importa el enrutador con lazy loading
 
 import "./css/index.css";
+import AuthProvider from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <RouterProvider router={router}/>
+        <AuthProvider>
+            <RouterProvider router={router}/>
+        </AuthProvider>
     </StrictMode>
 );
