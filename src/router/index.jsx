e7b51ccb3b68ea249f78b1ff022b4e560d08d0a9
RekZNoZ <rafikicroz@hotmail.com>
2025-01-15 23:12:00 +0100

    Update index.jsx

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -24,7 +24,7 @@ export const router = createBrowserRouter([
                 element: <Contacto/>,
             },
             {
-                path: "usuario",
+                path: "usuario/:username",
                 element: <PerfilUsuario/>,
             }
         ],
