57cc82361faaf4c5a488f73ffa88fb0c51fe7e79
RekZNoZ <rafikicroz@hotmail.com>
2025-01-19 22:28:19 +0100

    Update Mascota.jsx

--- src/pages/Mascota.jsx
+++ src/pages/Mascota.jsx
@@ -1,53 +1,26 @@
-import {useNavigate, useParams} from "react-router-dom";
+import {useLoaderData, useNavigate, useParams} from "react-router-dom";
 import "../css/mascota.css";
+import {useEffect, useState} from "react";
 
 export default function Mascota() {
 
-    const {id} = useParams();
+    const [animal, setAnimal] = useState(null);
     const navigate = useNavigate();
+    const cargaDeDatos = useLoaderData();
 
-    const animales = [
-        {
-            id: 1,
-            nombre: 'Max',
-            especie: 'Perro',
-            raza: 'Labrador',
-            edad: 3,
-            estadoSalud: 'Bueno',
-            duenio: 'Juan Pérez',
-            fechaRegistro: '01/15/2025'
-        },
-        {
-            id: 2,
-            nombre: 'Luna',
-            especie: 'Gato',
-            raza: 'Siamés',
-            edad: 2,
-            estadoSalud: 'Bueno',
-            duenio: 'Ana López',
-            fechaRegistro: '01/10/2025'
-        },
-        {
-            id: 3,
-            nombre: 'Rocky',
-            especie: 'Perro',
-            raza: 'Bulldog',
-            edad: 5,
-            estadoSalud: 'Regular',
-            duenio: 'Carlos Gómez',
-            fechaRegistro: '01/05/2025'
-        },
-    ];
+    useEffect(() => {
+        if (cargaDeDatos) {
+            setAnimal(cargaDeDatos);
+        }
+    }, [cargaDeDatos]);
 
-    const animal = animales.find((animal) => animal.id === parseInt(id));
-
-    if (!animal) {
+    if (animal === null) {
         return (
             <>
                 <div className="area1">
                     <div className="no-encontrado">
                         <h2>Mascota no encontrada</h2>
-                        <button onClick={() => navigate('/')}>Volver a la lista</button>
+                        <button onClick={() => navigate('/mascotas')}>Volver a la lista</button>
                     </div>
                 </div>
                 <div className="area2"></div>
