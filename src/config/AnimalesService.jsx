cb11e4d847aadd0dfa9a60e308bc9f375163d713
RekZNoZ <rafikicroz@hotmail.com>
2025-01-19 20:37:44 +0100

    Create AnimalesService.jsx

---
+++ src/config/AnimalesService.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:3000/animales/";
+
+export const obtenerAnimales = async () => {
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.error("Error al obtener animales:", error)
+        return []
+    }
+}
+
+export const agregarAnimal = async (animal) => {
+    try {
+        const response = await axios.post(API_URL, animal);
+        console.log("response", response)
+        return response.data;
+    } catch (error) {
+        console.error("Error al agregar animal:", error)
+        return {}
+    }
+}
