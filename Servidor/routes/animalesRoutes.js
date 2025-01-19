3e3bb8a34686de7495da3a239bbe8c556574bf93
RekZNoZ <rafikicroz@hotmail.com>
2025-01-19 22:28:23 +0100

    Update animalesRoutes.js

--- Servidor/routes/animalesRoutes.js
+++ Servidor/routes/animalesRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 const router = Router();
-import { editarAnimal, crearAnimal, obtenerAnimales, obtenerAnimalesPorNombre, eliminarAnimal } from '../controllers/animalesController.js';
+import { editarAnimal, crearAnimal, obtenerAnimales, obtenerAnimalesPorNombre, eliminarAnimal, obtenerAnimal } from '../controllers/animalesController.js';
 
 router.post('/', crearAnimal);
 router.get('/', obtenerAnimales);
+router.get('/:id', obtenerAnimal);
 router.get('/a/:nombre', obtenerAnimalesPorNombre); 
 router.put('/:id', editarAnimal); 
 router.delete('/', eliminarAnimal); 
