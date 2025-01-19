e22dc7abb3375d6432b34b63ff86d302b2677ca9
RekZNoZ <rafikicroz@hotmail.com>
2025-01-19 21:32:05 +0100

    Update AnimalesService.jsx

--- src/config/AnimalesService.jsx
+++ src/config/AnimalesService.jsx
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const API_URL = "http://localhost:3000/animales/";
 
-export const obtenerAnimales = async () => {
+export const obtenerAnimales = async (nombreUsuario) => {
     try {
-        const response = await axios.get(API_URL);
+        const response = await axios.get(`${API_URL}/a/${nombreUsuario}`);
         return response.data;
     } catch (error) {
         console.error("Error al obtener animales:", error)
